589e4992d6f43c5a2d6f62ea83af6158146ada69
watch14 <watchman1414@gmail.com>
2023-12-05 13:26:33 +0000

    fast_push

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """BaseModel"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel():
@@ -23,6 +24,7 @@ class BaseModel():
                     self.updated_at = datetime.strptime(value, time_form)
                 if key == "id":
                     self.id = value
+        models.storage.new(self)
 
     def __str__(self):
         """return string info"""
@@ -32,6 +34,7 @@ class BaseModel():
     def save(self):
         """ save / update """
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         """return a dictionary of the instance"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""FileStorage: serializes and deserializes"""
+import json
+import os
+import models
+
+
+class FileStorage ():
+    """FileStorage Class"""
+
+    def __init__(self):
+        """attributes"""
+        self.__file_path = "file.json"
+        self.__objects = {}
+
+    def all(self):
+        """returns __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """set obj"""
+        k_o = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[k_o] = obj
+
+    def save(self):
+        """Sereialize JSON"""
+        obj_dic = {}
+        for key in FileStorage.__objects.keys():
+            obj = FileStorage.__objects[key]
+            obj_dic[key] = obj.to_dict()
+
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(obj_dic, f)
+
+    def reload(self):
+        """Deserialize JSON"""
+        if (os.path.isfile(FileStorage.__file_path)):
+            with open(FileStorage.__file_path, "r") as f:
+                de_json = json.load(f)
+                for key, value in de_json.items():
+                    FileStorage.__objects[key] = eval(
+                            value['__class__'])(**value)
