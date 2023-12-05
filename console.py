51043668dd71505f7fc523d2edb04e504b015465
watch14 <watchman1414@gmail.com>
2023-12-05 09:10:19 +0000

    6 consol

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Cmd hbnbClass """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ cmd """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """" Quit hbnb """
+        return True
+
+    do_EOF = do_quit
+
+    def do_help(self, arg):
+        """" help hbnb """
+        cmd.Cmd.do_help(self, arg)
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
