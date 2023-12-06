793f433025ba7f036c78aa2322173f0782a7a460
watch14 <watchman1414@gmail.com>
2023-12-06 14:16:09 +0000

    fast_push

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+import unittest
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """test_base_model"""
+    def test_instance_creation(self):
+        """test"""
+        obj = BaseModel()
+        self.assertIsInstance(obj, BaseModel)
+        self.assertTrue(hasattr(obj, 'id'))
+        self.assertTrue(hasattr(obj, 'created_at'))
+        self.assertTrue(hasattr(obj, 'updated_at'))
+
+    def test_string_representation(self):
+        """test"""
+        obj = BaseModel()
+        string_representation = str(obj)
+        self.assertIn('BaseModel', string_representation)
+        self.assertIn(obj.id, string_representation)
+
+    def test_save_method(self):
+        """test"""
+        obj = BaseModel()
+        initial_updated_at = obj.updated_at
+        obj.save()
+        self.assertNotEqual(initial_updated_at, obj.updated_at)
+
+    def test_to_dict_method(self):
+        """test"""
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertIn('__class__', obj_dict)
+        self.assertIn('created_at', obj_dict)
+        self.assertIn('updated_at', obj_dict)
+
+    def test_deserialization_from_dict(self):
+        """test"""
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        new_obj = BaseModel(**obj_dict)
+        self.assertEqual(obj.id, new_obj.id)
+
+
+if __name__ == '__main__':
+    unittest.main()
