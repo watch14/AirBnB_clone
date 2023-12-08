0b5de955200fa891a1f2d6f2a72b0b10a927c492
watch14 <watchman1414@gmail.com>
2023-12-08 14:29:23 +0000

    fast_push

--- models/base_model.py
+++ models/base_model.py
@@ -43,3 +43,16 @@ class BaseModel():
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
+
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
