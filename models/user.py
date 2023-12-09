a64e2b51c10ed5b57d95c39cf710ceee07d90723
watch14 <watchman1414@gmail.com>
2023-12-09 16:51:48 +0000

    fast_push

--- models/user.py
+++ models/user.py
@@ -4,13 +4,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
-    """
+    """User"""
 
     email = ""
     password = ""
