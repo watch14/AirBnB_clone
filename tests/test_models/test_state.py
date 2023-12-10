6751aa021533a8c00a67afa7df1954f1716dc634
watch14 <watchman1414@gmail.com>
2023-12-10 13:34:37 +0000

    fast_push

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,30 +1,53 @@
 #!/usr/bin/python3
 """test"""
+
 import unittest
 from models.state import State
+from models import storage
 
 
 class TestState(unittest.TestCase):
     """test"""
+
     def setUp(self):
         """test"""
-        pass
+        self.state = State()
 
     def tearDown(self):
         """test"""
         pass
 
-    def test_state_attributes(self):
+    def test_attributes(self):
         """test"""
-        state = State()
-        state.name = "California"
+        self.assertTrue(hasattr(self.state, 'name'))
 
-        self.assertEqual(state.name, "California")
-
-    def test_state_instance_of_base_model(self):
+    def test_default_values(self):
         """test"""
-        state = State()
+        self.assertEqual(self.state.name, "")
+
+    def test_save_reload(self):
+        """test"""
+        self.state.name = "California"
+        self.state.save()
+        key = "{}.{}".format(self.state.__class__.__name__, self.state.id)
+        reloaded_state = storage.all()[key]
+        self.assertEqual(reloaded_state.name, "California")
+
+    def test_to_dict(self):
+        """test"""
+        self.state.name = "New York"
+        state_dict = self.state.to_dict()
+        self.assertEqual(state_dict['name'], "New York")
+        self.assertEqual(state_dict['__class__'], "State")
+        self.assertTrue('id' in state_dict)
+        self.assertTrue('created_at' in state_dict)
+        self.assertTrue('updated_at' in state_dict)
+
+    def test_created_at_updated_at(self):
+        """test"""
+        self.assertTrue(isinstance(self.state.created_at, datetime))
+        self.assertTrue(isinstance(self.state.updated_at, datetime))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
