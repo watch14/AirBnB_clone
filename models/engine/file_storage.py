aad0f000a40cfe86465f23cff4702b84be595036
watch14 <watchman1414@gmail.com>
2023-12-10 10:27:04 +0000

    fast_push

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" class FileStorage"""
+"""FileStorage: serializes and deserializes"""
 import json
 import uuid
 import os
@@ -13,31 +13,36 @@ from models.place import Place
 from models.review import Review
 
 
-class FileStorage:
-    """ construct """
+class FileStorage ():
+    """FileStorage Class"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ return dictionary objects """
+        """returns __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """ sets in dictionary"""
-        FileStorage.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
+        """set obj"""
+        k_o = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[k_o] = obj
 
     def save(self):
-        """ serialize"""
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as fname:
-            new_dict = {key: obj.to_dict() for key, obj in
-                        FileStorage.__objects.items()}
-            json.dump(new_dict, fname)
+        """Sereialize JSON"""
+        obj_dic = {}
+        for key in FileStorage.__objects.keys():
+            obj = FileStorage.__objects[key]
+            obj_dic[key] = obj.to_dict()
+
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(obj_dic, f)
 
     def reload(self):
-        """ Reload """
+        """Deserialize JSON"""
         if (os.path.isfile(FileStorage.__file_path)):
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fname:
-                l_json = json.load(fname)
-                for key, val in l_json.items():
+            with open(FileStorage.__file_path, "r") as f:
+                de_json = json.load(f)
+                for key, value in de_json.items():
                     FileStorage.__objects[key] = eval(
-                        val['__class__'])(**val)
+                            value['__class__'])(**value)
