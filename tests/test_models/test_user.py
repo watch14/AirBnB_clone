2623050d2bd83b2d5003942b65d42b30f5734e65
watch14 <watchman1414@gmail.com>
2023-12-10 13:22:21 +0000

    fast_push

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,38 +1,70 @@
 #!/usr/bin/python3
-"""user tests"""
+"""Test for User"""
 import unittest
 from models.user import User
-from models.base_model import BaseModel
+from datetime import datetime
+from models import storage
 
 
 class TestUser(unittest.TestCase):
-    """user test"""
     def setUp(self):
-        """user test"""
+        """Set up for testing."""
         self.user = User()
 
     def tearDown(self):
-        """user test"""
+        """Clean test files."""
         pass
 
-    def test_user_attributes(self):
-        """user test"""
-        user = User()
-        user.email = "test@example.com"
-        user.password = "password123"
-        user.first_name = "John"
-        user.last_name = "Doe"
+    def test_attributes(self):
+        """Test user attributes."""
+        self.assertTrue(hasattr(self.user, 'email'))
+        self.assertTrue(hasattr(self.user, 'password'))
+        self.assertTrue(hasattr(self.user, 'first_name'))
+        self.assertTrue(hasattr(self.user, 'last_name'))
 
-        self.assertEqual(user.email, "test@example.com")
-        self.assertEqual(user.password, "password123")
-        self.assertEqual(user.first_name, "John")
-        self.assertEqual(user.last_name, "Doe")
+    def test_default_values(self):
+        """Test default values."""
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
 
-    def test_user_instance_of_base_model(self):
-        """user test"""
-        user = User()
-        self.assertIsInstance(user, BaseModel)
+    def test_save_reload(self):
+        """Test if User saves and reloads correctly."""
+        self.user.email = "test@example.com"
+        self.user.password = "password123"
+        self.user.first_name = "John"
+        self.user.last_name = "Doe"
+        self.user.save()
+        key = "{}.{}".format(self.user.__class__.__name__, self.user.id)
+        reloaded_user = storage.all()[key]
+        self.assertEqual(reloaded_user.email, "test@example.com")
+        self.assertEqual(reloaded_user.password, "password123")
+        self.assertEqual(reloaded_user.first_name, "John")
+        self.assertEqual(reloaded_user.last_name, "Doe")
+
+    def test_to_dict(self):
+        """Test if to_dict method produces correct dictionary."""
+        self.user.email = "test@example.com"
+        self.user.password = "password123"
+        self.user.first_name = "John"
+        self.user.last_name = "Doe"
+        user_dict = self.user.to_dict()
+        self.assertEqual(user_dict['email'], "test@example.com")
+        self.assertEqual(user_dict['password'], "password123")
+        self.assertEqual(user_dict['first_name'], "John")
+        self.assertEqual(user_dict['last_name'], "Doe")
+        self.assertEqual(user_dict['__class__'], "User")
+        self.assertTrue('id' in user_dict)
+        self.assertTrue('created_at' in user_dict)
+        self.assertTrue('updated_at' in user_dict)
+
+    def test_created_at_updated_at(self):
+        """Test if created_at and updated_at are instances of datetime."""
+
+        self.assertTrue(isinstance(self.user.created_at, datetime))
+        self.assertTrue(isinstance(self.user.updated_at, datetime))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
